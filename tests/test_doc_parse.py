4237b21991e892b1a3e7f79cf7e4c73d0d313771
FasterSpeeding <lucina@lmbyrne.dev>
2023-07-26 23:18:59 +0100

    Some fixes

--- tests/test_doc_parse.py
+++ tests/test_doc_parse.py
@@ -103,7 +103,7 @@ def test_as_slash_command_when_all_args_passed():
         is_global=False,
         name="overridden_name",
         nsfw=True,
-        sort_otions=False,
+        sort_options=False,
         validate_arg_keys=False,
     )
     async def command(ctx: tanjun.abc.Context) -> None:
@@ -111,9 +111,9 @@ def test_as_slash_command_when_all_args_passed():
 
     builder = command.build()
 
-    assert command._always_defer is True
+    assert command._always_defer is True  # pyright: ignore[reportPrivateUsage]
     assert command.default_member_permissions == 123
-    assert builder._default_to_ephemeral is True
+    assert command._default_to_ephemeral is True # pyright: ignore[reportPrivateUsage]
     assert builder.description == command.description == "Meow meow meow meow!"
     assert command.is_dm_enabled is False
     assert command.is_global is False
@@ -128,9 +128,9 @@ def test_as_slash_command_with_arg_defaults():
 
     builder = command.build()
 
-    assert command._always_defer is False
+    assert command._always_defer is False # pyright: ignore[reportPrivateUsage]
     assert command.default_member_permissions is None
-    assert builder._default_to_ephemeral is False
+    assert command._default_to_ephemeral is False # pyright: ignore[reportPrivateUsage]
     assert command.is_dm_enabled is True
     assert command.is_global is True
     assert command.is_nsfw is False
