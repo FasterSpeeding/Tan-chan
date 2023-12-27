bcc7326c8b6df5e453e8659e93a2f521b21bd022
FasterSpeeding <lucina@lmbyrne.dev>
2023-12-27 22:26:21 +0000

    Type checking fixes

--- tests/test_doc_parse.py
+++ tests/test_doc_parse.py
@@ -1370,13 +1370,13 @@ def test_ignores_docless_typed_dict():
 
 @pytest.mark.skipif(not TANJUN_SUPPORTS_TYPED_DICT, reason="Tanjun version doesn't support typed dict parsing")
 def test_ignores_typed_dict_has_standard_doc():
-    typed_dict = typing_extensions.TypedDict(
+    TypedDict = typing_extensions.TypedDict(
         "TypedDict", {"dump": annotations.Bool, "truck": typing_extensions.NotRequired[annotations.Bool]}
     )
 
     @tanchan.doc_parse.with_annotated_args
     @tanchan.doc_parse.as_slash_command()
-    async def command(ctx: tanjun.abc.Context, **kwargs: typing_extensions.Unpack[typed_dict]) -> None:
+    async def command(ctx: tanjun.abc.Context, **kwargs: typing_extensions.Unpack[TypedDict]) -> None:
         """Command.
 
         Parameters
@@ -1578,10 +1578,10 @@ def test_when_typed_dict_has_no_doc_and_cant_detect_doc_style():
 
 @pytest.mark.skipif(not TANJUN_SUPPORTS_TYPED_DICT, reason="Tanjun version doesn't support typed dict parsing")
 def test_when_standard_typed_dict_doc_and_cant_detect_doc_style():
-    typed_dict = typing.TypedDict("TypedDict", {})
+    TypedDict = typing.TypedDict("TypedDict", {})
 
     @tanchan.doc_parse.as_slash_command()
-    async def command(ctx: tanjun.abc.Context, **kwargs: typing_extensions.Unpack[typed_dict]) -> None:
+    async def command(ctx: tanjun.abc.Context, **kwargs: typing_extensions.Unpack[TypedDict]) -> None:
         """Description.
 
         Not empty.
